e7cc2f7c44ee931f170e321a23fe8931f4bfc304
차요셉 <jscha@raoncorp.com>
2022-07-28 23:09:29 +0900

    [백준]5397/키로거 다시코딩

--- 백준/스택/5397:키로커.swift
+++ 백준/스택/5397:키로커.swift
@@ -5,4 +5,30 @@
 //  Created by Joseph Cha on 2022/07/22.
 //
 
-import Foundation
+let testCase = Int(readLine()!)!
+
+for _ in 0..<testCase {
+    let input = readLine()!.map{String($0)}
+    var frontArray: [String] = []
+    var backArray: [String] = []
+    input.forEach { str in
+        switch str {
+        case "<":
+            if !frontArray.isEmpty {
+                backArray.append(frontArray.popLast()!)
+            }
+        case ">":
+            if !backArray.isEmpty {
+                frontArray.append(backArray.popLast()!)
+            }
+        case "-":
+            if !frontArray.isEmpty {
+                frontArray.popLast()!
+            }
+        default:
+            frontArray.append(str)
+        }
+    }
+    backArray.reverse()
+    print(frontArray.joined()+backArray.joined())
+}
