add59b245770ca427d68bdf113b2192957e661b7
차요셉 <jscha@raoncorp.com>
2022-09-29 23:52:07 +0900

    [백준] 1991 / 트리순회

---
+++ 백준/트리/1991: 트리 순회.swift
@@ -0,0 +1,34 @@
+//
+//  1991: 트리 순회.swift
+//  코테연습장
+//
+//  Created by Joseph Cha on 2022/09/29.
+//
+
+import Foundation
+
+let count = Int(readLine()!)!
+var results: [String] = ["", "", ""]
+var tree: [String:[String]] = [:]
+for _ in 0..<count {
+    let input = readLine()!.split { $0 == " " }.map { String($0) }
+    tree.updateValue([input[1], input[2]], forKey: input[0])
+}
+
+func dfs(_ node: String) {
+    if node == "." {
+        return
+    }
+    
+    results[0] += node
+    dfs(tree[node]![0])
+    results[1] += node
+    dfs(tree[node]![1])
+    results[2] += node
+}
+
+dfs("A")
+
+for result in results {
+    print(result)
+}
